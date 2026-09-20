877664cfdb2dc0f41603ca57bb1677d408e406cf
the-AbdulRafik <abdulrafiks7860@gmail.com>
2026-09-20 22:35:54 +0530

    feat(api): expose detector modules from package entrypoint

---
+++ src/detector/index.ts
@@ -0,0 +1,14 @@
+export {
+  detectStackSignature,
+  detectStackSignatureSync,
+} from './stack-detector.js';
+
+export {
+  detectNode,
+  detectNodeSync,
+  detectPackageManager,
+  detectPackageManagerSync,
+  detectFramework,
+  getRunScriptCommand,
+  type PackageJson,
+} from './node-detector.js';
